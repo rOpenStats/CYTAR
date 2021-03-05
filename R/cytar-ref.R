c225440660dcbd7f0ec83647ba0f4ede0280a5d0
Blanca Caffaro <m.blanca.caffaro@gmail.com>
2021-03-05 15:04:19 -0300

    [bugfix] col_types

--- R/cytar-ref.R
+++ R/cytar-ref.R
@@ -14,15 +14,10 @@ CYTARCategoriaConicetRef <- R6Class("CYTARCategoriaConicetRef",
     super$initialize(data.url = "https://datasets.datos.mincyt.gob.ar/dataset/06ae9728-c376-47bd-9c41-fbdca68707c6/resource/c72c9f88-d9ef-4349-bb20-5c9a1aca5d67/download/ref_categoria_conicet.csv",
                      data.filename = "ref_categoria_conicet.csv",
                      col.types =
-                      cols(
-                       disciplina_id = col_integer(),
-                       gran_area_codigo = col_integer(),
-                       gran_area_descripcion = col_character(),
-                       area_codigo = col_integer(),
-                       area_descripcion = col_character(),
-                       disciplina_codigo = col_character(),
-                       disciplina_descripcion = col_character()
-                      )
+                       cols(
+                         categoria_conicet_id = col_double(),
+                         categoria_conicet_descripcion = col_character()
+                       )
     )
     self
    },
@@ -49,16 +44,12 @@ CYTARTipoPersonalRef <- R6Class("CYTARTipoPersonalRef",
     initialize = function(){
        super$initialize(data.url = "https://datasets.datos.mincyt.gob.ar/dataset/06ae9728-c376-47bd-9c41-fbdca68707c6/resource/90d498e9-5a96-46df-b51d-956b5702bb02/download/ref_tipo_personal.csv",
                         data.filename = "ref_tipo_personal.csv",
-                        col.types =
-                           cols(
-                              disciplina_id = col_integer(),
-                              gran_area_codigo = col_integer(),
-                              gran_area_descripcion = col_character(),
-                              area_codigo = col_integer(),
-                              area_descripcion = col_character(),
-                              disciplina_codigo = col_character(),
-                              disciplina_descripcion = col_character()
-                           )
+                        col.types = cols(
+                          tipo_personal_id = col_double(),
+                          tipo_personal_descripcion = col_character()
+                        )
+
+
        )
        self
     },
@@ -87,16 +78,16 @@ CYTARDisciplinasRef <- R6Class("CYTARDisciplinasRef",
        super$initialize(data.url = "https://datasets.datos.mincyt.gob.ar/dataset/06ae9728-c376-47bd-9c41-fbdca68707c6/resource/ab280873-8206-4e16-a020-064a9243277f/download/ref_disciplina.csv",
                         data.filename = "ref_disciplina.csv",
                         col.types =
-                           cols(
-                              disciplina_id = col_integer(),
-                              gran_area_codigo = col_integer(),
-                              gran_area_descripcion = col_character(),
-                              area_codigo = col_integer(),
-                              area_descripcion = col_character(),
-                              disciplina_codigo = col_character(),
-                              disciplina_descripcion = col_character()
-                           )
-       )
+                          cols(
+                            disciplina_id = col_double(),
+                            gran_area_codigo = col_double(),
+                            gran_area_descripcion = col_character(),
+                            area_codigo = col_double(),
+                            area_descripcion = col_character(),
+                            disciplina_codigo = col_character(),
+                            disciplina_descripcion = col_character()
+                          )
+                                 )
        self
     },
     checkConsolidatedFields = function(fields){
